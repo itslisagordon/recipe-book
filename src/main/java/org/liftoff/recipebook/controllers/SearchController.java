8a7c2786750ca9564a928340bf63e29d267088ae
Joseph Hendricks <jah8w4@mail.missouri.edu>
2020-12-18 17:01:20 -0600

    search in navbar

---
+++ src/main/java/org/liftoff/recipebook/controllers/SearchController.java
@@ -0,0 +1,28 @@
+package org.liftoff.recipebook.controllers;
+
+import org.liftoff.recipebook.models.User;
+import org.liftoff.recipebook.models.UserData;
+import org.liftoff.recipebook.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SearchController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("search")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+
+        List<User> results = new ArrayList<>();
+        results = UserData.findUser(searchTerm, userRepository.findAll());
+        model.addAttribute("results", results);
+        return "search";
+    }
+}
