3221a83630c2bd703ae5f05ead5aa12c5da1fe28
Lisa Gordon <lg.official94@gmail.com>
2020-12-21 01:14:30 -0600

    test

--- src/main/java/org/liftoff/recipebook/controllers/ProfileController.java
+++ src/main/java/org/liftoff/recipebook/controllers/ProfileController.java
@@ -1,2 +1,14 @@
-package org.liftoff.recipebook.controllers;public class ProfileController {
+package org.liftoff.recipebook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("profile")
+    public String displayProfile() {
+        return "";
+    }
+
 }
