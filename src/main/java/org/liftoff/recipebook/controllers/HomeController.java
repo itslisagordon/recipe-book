96e0d345cbaeb6914b705672e750ca2619933e74
Joseph Hendricks <jah8w4@mail.missouri.edu>
2021-01-29 11:05:41 -0600

    'my profile' working on home page

--- src/main/java/org/liftoff/recipebook/controllers/HomeController.java
+++ src/main/java/org/liftoff/recipebook/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package org.liftoff.recipebook.controllers;
 
+import org.liftoff.recipebook.models.User;
+import org.liftoff.recipebook.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        User sessionUser = authenticationController.getUserFromSession(session);
+        int userId = sessionUser.getId();
+
+        model.addAttribute("profile", userRepository.findById(userId).get());
         return "index";
     }
 
