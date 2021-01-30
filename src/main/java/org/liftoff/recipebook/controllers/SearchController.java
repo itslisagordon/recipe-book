9145326290189fc451ae401e58cc4110d8ea7e50
Ekirkman <33643736+Ekirkman@users.noreply.github.com>
2021-01-30 17:12:41 -0600

    Update SearchController.java

--- src/main/java/org/liftoff/recipebook/controllers/SearchController.java
+++ src/main/java/org/liftoff/recipebook/controllers/SearchController.java
@@ -1,14 +1,16 @@
 package org.liftoff.recipebook.controllers;
 
-import org.liftoff.recipebook.models.data.UserRepository;
 import org.liftoff.recipebook.models.User;
 import org.liftoff.recipebook.models.UserData;
+import org.liftoff.recipebook.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +19,20 @@ public class SearchController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    AuthenticationController authenticationController;
+
     @PostMapping("search")
-    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, HttpServletRequest request){
 
         List<User> results = new ArrayList<>();
         results = UserData.findUser(searchTerm, userRepository.findAll());
+
+        HttpSession session = request.getSession();
+        User sessionUser = authenticationController.getUserFromSession(session);
+        int userId = sessionUser.getId();
+
+        model.addAttribute("profile", userRepository.findById(userId).get());
         model.addAttribute("results", results);
         model.addAttribute("searchTerm", searchTerm);
         return "search";
