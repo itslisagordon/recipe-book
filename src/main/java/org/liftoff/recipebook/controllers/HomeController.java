84635406e07a805a07e6b840f74e3ea21cee11ad
Ekirkman <33643736+Ekirkman@users.noreply.github.com>
2021-01-30 17:08:56 -0600

    Update HomeController.java

--- src/main/java/org/liftoff/recipebook/controllers/HomeController.java
+++ src/main/java/org/liftoff/recipebook/controllers/HomeController.java
@@ -1,17 +1,36 @@
 package org.liftoff.recipebook.controllers;
 
+import org.liftoff.recipebook.models.User;
+import org.liftoff.recipebook.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
-
 public class HomeController {
 
-    @GetMapping
-    public String index() {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @RequestMapping("/")
+    public String index(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        User sessionUser = authenticationController.getUserFromSession(session);
+        int userId = sessionUser.getId();
+
+        model.addAttribute("profile", userRepository.findById(userId).get());
         return "index";
     }
 
-
-
+//test
+    
 }
