c63fdb7a5fb1dbc32d88ce448670f38cdcda8df5
Carl B <c3jb@sbcglobal.net>
2021-02-01 18:23:01 -0600

    added connectivity for create-recipe.html & create-category.html

--- src/main/java/org/liftoff/recipebook/controllers/RecipeController.java
+++ src/main/java/org/liftoff/recipebook/controllers/RecipeController.java
@@ -1,9 +1,11 @@
 package org.liftoff.recipebook.controllers;
 
+import org.liftoff.recipebook.models.User;
 import org.liftoff.recipebook.models.data.RecipeCategoryRepository;
 import org.liftoff.recipebook.models.data.RecipeRepository;
 import org.liftoff.recipebook.models.Recipe;
 import org.liftoff.recipebook.models.RecipeCategory;
+import org.liftoff.recipebook.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 
 
 @Controller
@@ -23,10 +27,21 @@ public class RecipeController {
     @Autowired
     private RecipeCategoryRepository recipeCategoryRepository;
 
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/create-recipe")
-    public String renderCreateRecipeForm(Model model){
+    public String renderCreateRecipeForm(Model model, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        User sessionUser = authenticationController.getUserFromSession(session);
+        int userId = sessionUser.getId();
+
         model.addAttribute("recipe",new Recipe());
         model.addAttribute("categories", recipeCategoryRepository.findAll());
+        model.addAttribute("profile", userRepository.findById(userId).get());
         return "create-recipe";
     }
 
@@ -48,6 +63,6 @@ public class RecipeController {
         recipe.setIngredients(hiddenIngredients);
         recipe.setCategory(category);
         recipeRepository.save(recipe);
-        return "redirect:";
+        return "redirect:/profile/{userId}";
     }
 }
